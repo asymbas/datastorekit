0520090e40d43d3931af4a355c34f10798a6d64f
Asymbas <asymbascore@icloud.com>
2026-03-26 14:39:30 -0600

    Add DataStoreOptions property

--- Sources/DataStoreCore/Configuration/DataStoreOptions.swift
+++ Sources/DataStoreCore/Configuration/DataStoreOptions.swift
@@ -22,7 +22,7 @@ public struct DataStoreOptions: OptionSet, Sendable {
 }
 
 extension DataStoreOptions {
-    nonisolated private static var allowedMask: Int { (1 << 21) - 1 } // bits 0...20
+    nonisolated private static var allowedMask: Int { (1 << 22) - 1 } // bits 0...21
     
     nonisolated private init(uncheckedRawValue: Int) {
         self.rawValue = uncheckedRawValue
@@ -38,80 +38,84 @@ public extension DataStoreOptions {
     nonisolated static var eraseDatabaseOnSetup: Self {
         .init(uncheckedRawValue: 1 << 1)
     }
+    /// Sets up the database in a temporary directory.
+    nonisolated static var temporary: Self {
+        .init(uncheckedRawValue: 1 << 2)
+    }
     /// Logs additional information.
     nonisolated static var useDetailedLogging: Self {
-        .init(uncheckedRawValue: 1 << 2)
+        .init(uncheckedRawValue: 1 << 3)
     }
     /// Increases the logging frequency.
     nonisolated static var useVerboseLogging: Self {
-        .init(uncheckedRawValue: 1 << 3)
-    }
-    /// Changes the log level of save requests from `info` to `notice`.
-    nonisolated static var logSaveRequest: Self {
         .init(uncheckedRawValue: 1 << 4)
     }
-    /// Logs the translated `FetchDescriptor` used to fetch from the data store.
-    nonisolated static var logTranslatedFetchDescriptor: Self {
+    /// Changes the log level of save requests from `info` to `notice`.
+    nonisolated static var logSaveRequests: Self {
         .init(uncheckedRawValue: 1 << 5)
+    }
+    /// Logs the translated `FetchDescriptor` used to fetch from the data store.
+    nonisolated static var logTranslatedFetchDescriptors: Self {
+        .init(uncheckedRawValue: 1 << 6)
     }
     /// Create snapshots from a fetch request synchronously.
     nonisolated static var synchronouslyCreateSnapshots: Self {
-        .init(uncheckedRawValue: 1 << 6)
+        .init(uncheckedRawValue: 1 << 7)
     }
     /// Only allow the `ModelManager` to maintain references to the snapshot caches.
     nonisolated static var centralizedSnapshotCaching: Self {
-        .init(uncheckedRawValue: 1 << 7)
+        .init(uncheckedRawValue: 1 << 8)
     }
     /// Snapshots will no longer be cached into `ModelManager` and/or any `SnapshotRegistry`.
     nonisolated static var disableSnapshotCaching: Self {
-        .init(uncheckedRawValue: 1 << 8)
+        .init(uncheckedRawValue: 1 << 9)
     }
     /// Previously cached snapshots will not be reused as related models and prefetched relationships.
     nonisolated static var disableImplicitPrefetchingUsingCaches: Self {
-        .init(uncheckedRawValue: 1 << 9)
+        .init(uncheckedRawValue: 1 << 10)
     }
     /// Do not include related backing data from being requested in the predicate.
     nonisolated static var disableImplicitPrefetchingFromPredicate: Self {
-        .init(uncheckedRawValue: 1 << 10)
+        .init(uncheckedRawValue: 1 << 11)
     }
     /// Always fetch foreign keys from the data store when acquiring references for to-many relationships.
     nonisolated static var disableReferenceCaching: Self {
-        .init(uncheckedRawValue: 1 << 11)
+        .init(uncheckedRawValue: 1 << 12)
     }
     /// Prevents hashing the fetch request for storing and reusing its fetch result.
     nonisolated static var disablePredicateCaching: Self {
-        .init(uncheckedRawValue: 1 << 12)
+        .init(uncheckedRawValue: 1 << 13)
     }
     /// Restricts key paths to only use from the schema.
     nonisolated static var disableKeyPathVariants: Self {
-        .init(uncheckedRawValue: 1 << 13)
+        .init(uncheckedRawValue: 1 << 14)
     }
     /// Overwrite the encoded SwiftData `Schema` data.
     nonisolated static var forceSchemaOverwrite: Self {
-        .init(uncheckedRawValue: 1 << 14)
+        .init(uncheckedRawValue: 1 << 15)
     }
     /// Prevents any schema migrations checking.
     nonisolated static var disableSchemaMigrations: Self {
-        .init(uncheckedRawValue: 1 << 15)
+        .init(uncheckedRawValue: 1 << 16)
     }
     /// Prevents automatic execution of any lightweight schema migrations.
     nonisolated static var disableLightweightSchemaMigrations: Self {
-        .init(uncheckedRawValue: 1 << 16)
+        .init(uncheckedRawValue: 1 << 17)
     }
     /// Skips recording and managing transactions in persistent history tracking.
     nonisolated static var disablePersistentHistoryTracking: Self {
-        .init(uncheckedRawValue: 1 << 17)
+        .init(uncheckedRawValue: 1 << 18)
     }
     /// Maintain inline transaction history and do not copy it externally.
     nonisolated static var neverArchiveTransactionHistory: Self {
-        .init(uncheckedRawValue: 1 << 18)
+        .init(uncheckedRawValue: 1 << 19)
     }
     /// Checks if `PRAGMA foreign_keys` is enabled and runs `PRAGMA foreign_key_check` for each table on setup.
     nonisolated static var verifyForeignKeysOnSetup: Self {
-        .init(uncheckedRawValue: 1 << 19)
+        .init(uncheckedRawValue: 1 << 20)
     }
     /// Prevents throwing any errors when the configuration is being validated.
     nonisolated static var ignoreStoreValidationErrors: Self {
-        .init(uncheckedRawValue: 1 << 20)
+        .init(uncheckedRawValue: 1 << 21)
     }
 }
