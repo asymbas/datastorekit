1eae04b2363b516385219c8df7fe12a66c395922
Asymbas <asymbascore@icloud.com>
2026-03-26 15:23:09 -0600

    Remove AsyncAlgorithms dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
         .library(name: "DataStoreKit", targets: ["DataStoreKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-async-algorithms", exact: "1.0.0"),
         .package(url: "https://github.com/apple/swift-collections.git", "1.2.0"..<"1.3.0"),
         .package(url: "https://github.com/apple/swift-log.git", "1.6.4"..<"1.8.0"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", "1.1.0"..<"1.2.0")
@@ -30,7 +29,6 @@ let package = Package(
                 "DataStoreSupport",
                 "SQLiteHandle",
                 "SQLiteStatement",
-                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
                 .product(name: "Collections", package: "swift-collections"),
                 .product(name: "Logging", package: "swift-log")
             ],
@@ -45,7 +43,6 @@ let package = Package(
                 "DataStoreSupport",
                 "SQLiteHandle",
                 "SQLiteStatement",
-                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
                 .product(name: "Collections", package: "swift-collections"),
                 .product(name: "Logging", package: "swift-log")
             ],
