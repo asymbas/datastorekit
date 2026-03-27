90ae58852cb515d1c758d94bb4372587b62cb2ca
Asymbas <asymbascore@icloud.com>
2026-03-26 19:49:30 -0600

    Update predicate expressions tests

--- Tests/DataStoreKitTests/PredicateExpressionsTests.swift
+++ Tests/DataStoreKitTests/PredicateExpressionsTests.swift
@@ -11,6 +11,7 @@ import DataStoreKit
 import Foundation
 import Logging
 import Testing
+import SQLiteStatement
 import SwiftData
 
 @Suite("PredicateExpressions")
@@ -21,11 +22,16 @@ struct PredicateExpressionsTests {
     
     init() throws {
         _ = logging
-        self.schema = Schema([Base.self])
+        let types: [any PersistentModel.Type] = [
+            Base.self,
+            RawValue.self,
+            EntitySQLPassthrough.self
+        ]
+        self.schema = Schema(types)
         var configuration = DatabaseConfiguration.transient(
-            types: [Base.self],
+            types: types,
             schema: schema,
-            options: .disableSnapshotCaching
+            options: [.disableSnapshotCaching, .useDetailedLogging, .useVerboseLogging]
         )
         configuration.configurations[.predicate] = SQLPredicateTranslatorOptions([
             .generateSQLStatement,
@@ -38,6 +44,69 @@ struct PredicateExpressionsTests {
     }
     
     @Model class Base {
+        @Attribute(.unique) var id: String
+        
+        init(id: String) {
+            self.id = id
+        }
+    }
+    
+    @Model class RawValue {
+        @Attribute(.unique) var id: String
+        @Attribute var color: Color
+        @Attribute var colors: Set<Color>
+        @Attribute var shape: Shape
+        @Attribute var shapes: Set<Shape>
+        @Attribute var style: Style
+        
+        init(
+            id: String = UUID().uuidString,
+            color: Color = .red,
+            colors: Set<Color> = [],
+            shape: Shape = .rectangle,
+            shapes: Set<Shape> = [],
+            style: Style = []
+        ) {
+            self.id = id
+            self.color = color
+            self.colors = colors
+            self.shape = shape
+            self.shapes = shapes
+            self.style = style
+        }
+        
+        enum Color: String, CaseIterable, Codable, Equatable, Hashable {
+            case red
+            case green
+            case blue
+        }
+        
+        struct Shape: CaseIterable, Codable, Equatable, Hashable, RawRepresentable {
+            static let allCases: Set<Self> = [.rectangle, .square, .circle, .triangle]
+            static let rectangle: Self = .init(rawValue: 0)
+            static let square: Self = .init(rawValue: 1)
+            static let circle: Self = .init(rawValue: 2)
+            static let triangle: Self = .init(rawValue: 3)
+            let rawValue: UInt
+            
+            init(rawValue: Self.RawValue) {
+                self.rawValue = rawValue
+            }
+        }
+        
+        struct Style: CaseIterable, Codable, OptionSet {
+            static let allCases: [Self] = [.fill, .stroke]
+            static let fill: Self = .init(rawValue: 0)
+            static let stroke: Self = .init(rawValue: 1)
+            let rawValue: Int32
+            
+            init(rawValue: Self.RawValue) {
+                self.rawValue = rawValue
+            }
+        }
+    }
+    
+    @Model class EntitySQLPassthrough: SQLPassthrough {
         @Attribute(.unique) var id: String
         
         init(id: String) {
@@ -71,9 +140,117 @@ struct PredicateExpressionsTests {
         let descriptor = FetchDescriptor(predicate: #Predicate<Base> {
             $0.id == id
         })
-        var translator = SQLPredicateTranslator<Base>.init(schema: schema)
+        var translator = SQLPredicateTranslator<Base>(schema: schema)
         let translation = try translator.translate(descriptor)
         #expect(translation.statement.bindings.contains(where: { ($0 as? SQLValue) == SQLValue(any: id) }))
+        let models = try modelContext.fetch(descriptor)
+        #expect(models.count == 1)
+    }
+    
+    @Test("Use struct and enum with constant or case raw values")
+    func rawValue() async throws {
+        let testStructShape = RawValue.Shape.allCases.randomElement()!
+        let testEnumColor = RawValue.Color.allCases.randomElement()!
+        let testStructShapes = Array(RawValue.Shape.allCases)
+            .shuffled()
+            .prefix(Int.random(in: 0...RawValue.Shape.allCases.count))
+        let testEnumColors = Array(RawValue.Color.allCases)
+            .shuffled()
+            .prefix(Int.random(in: 0...RawValue.Color.allCases.count))
+        let modelContext = ModelContext(modelContainer)
+        try seed(into: modelContext, models: [
+            RawValue(id: "test-struct", shape: testStructShape, shapes: Set(testStructShapes)),
+            RawValue(id: "test-enum", color: testEnumColor, colors: Set(testEnumColors))
+        ])
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-struct" && $0.shape == testStructShape
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+            #expect(models.count == 1)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-struct" && $0.shape.rawValue == testStructShape.rawValue
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+            #expect(models.count == 1)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-struct" && $0.shapes.contains(testStructShape)
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-struct" && $0.shapes.contains(where: { shape in
+                    shape.rawValue == testStructShape.rawValue
+                })
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-enum" && $0.color == testEnumColor
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+            #expect(models.count == 1)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-enum" && $0.color.rawValue == testEnumColor.rawValue
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+            #expect(models.count == 1)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-enum" && $0.colors.contains(testEnumColor)
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+        }
+        do {
+            let descriptor = FetchDescriptor(predicate: #Predicate<RawValue> {
+                $0.id == "test-enum" && $0.colors.contains(where: { shape in
+                    shape.rawValue == testEnumColor.rawValue
+                })
+            })
+            var translator = SQLPredicateTranslator<RawValue>(schema: schema)
+            let translation = try translator.translate(descriptor)
+            let models = try modelContext.fetch(descriptor)
+        }
+    }
+    
+    @Test("SQLPassthrough")
+    func sqlPassthrough() async throws {
+        let id = UUID().uuidString
+        let modelContext = ModelContext(modelContainer)
+        try seed(into: modelContext, models: [EntitySQLPassthrough(id: id)])
+        let sql = SQL {
+            "\"id\" = '\(id)'"
+        }
+        let descriptor = FetchDescriptor(predicate: #Predicate<EntitySQLPassthrough> {
+            $0.sql == sql
+        })
+        var translator = SQLPredicateTranslator<EntitySQLPassthrough>(schema: schema)
+        let translation = try translator.translate(descriptor)
+        #expect(translation.statement.sql.contains(id))
+        #expect(translation.statement.bindings.isEmpty)
         let models = try modelContext.fetch(descriptor)
         #expect(models.count == 1)
     }
